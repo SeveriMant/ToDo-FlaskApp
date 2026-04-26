562cde07d11eceff7835083120edfcfc2d8d80ea
SeveriMant <severi.mantysaari@gmail.com>
2026-04-26 20:16:29 +0300

    Updated version

--- todo-flask-app/app.py
+++ todo-flask-app/app.py
@@ -1,4 +1,5 @@
-from App import Flask, render_template, request, redirect
+
+from flask import Flask, render_template, request, redirect, make_response
 
 app = Flask(__name__)
 
@@ -18,7 +19,7 @@ def add():
 
 
 @app.route("/delete/<int:task_id>")
-def delete():
+def delete(task_id):
     if 0 <= task_id < len(tasks):
         tasks.pop(task_id)
     return redirect("/")
@@ -30,6 +31,20 @@ def complete(task_id):
         tasks[task_id]["done"] = not tasks[task_id]["done"]
     return redirect("/")
 
+@app.route("/save", methods=["POST"])
+def save():
+    filename = request.form.get("filename", "tasks.txt")
+    if not filename.endswith(".txt"):
+        filename += ".txt"
+
+    task_str =""
+    for task in tasks:
+        task_str += f"{task["task"]} {("✅" if task["done"] else "")}\n"
+
+    response = make_response(task_str)
+    response.headers["Content-Disposition"] = f"attachment; filename={filename}"
+    response.headers["Content-Type"] = "text/plain"
+    return response
 
 if __name__ == "__main__":
     app.run(debug=True)
